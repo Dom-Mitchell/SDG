929e308d32c30a1c463b99617981c38e56519c1b
Domenick Mitchell <69612428+Domanator13@users.noreply.github.com>
2021-07-28 12:24:47 -0400

    Add files via upload

---
+++ Week 2 -- Csharp/HelloDotNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            var name = "Dom Mitchell";
+            var userName = "";
+
+            // My Greeting
+            Console.WriteLine($"\nHello World, my name is {name}!\n");
+
+            // User Greeting
+            Console.WriteLine("What is your name?");
+            userName = Console.ReadLine();
+            Console.WriteLine($"\nIts a pleasure to meet you, {userName}!\n");
+        }
+    }
+}
