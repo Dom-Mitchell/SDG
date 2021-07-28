c7ded75fb47d7c729e73867067c9f3438365df84
Domenick Mitchell <69612428+Domanator13@users.noreply.github.com>
2021-07-28 12:28:38 -0400

    Add files via upload

---
+++ Week 2 -- Csharp/OurDotnetApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OurDotnetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Samantha";
+            Console.WriteLine("Welcome to SDG " + name + ".");
+
+        }
+    }
+}
